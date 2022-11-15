d6f6b73bb343e315f76914f3dcd8c5364aa48b82
Neider Urbano <jjuly8julianur@gmail.com>
2022-11-14 21:52:59 -0500

    actions test github

--- countries/tests.py
+++ countries/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from countries.models import Country
 
-# Create your tests here.
+
+class CountryTest(TestCase):
+    def test_valid_country(self):
+        self.assertEqual(2, 1)
